2441a46f8b614158db10d6186bea7344b559d92f
guzuligo <guzuligo@gmail.com>
2020-08-07 08:47:06 +0300

    Added default orientation

--- ResolutionTools.js
+++ ResolutionTools.js
@@ -1,17 +1,20 @@
 //For questions, refer to guzuligo at gmail dot com
-//Version 1.0.1
+//Version 1.0.2
 var ResolutionTools = pc.createScript('resolutionTools');
 
-ResolutionTools.attributes.add("ratio",{title:"Pixel Ratio",type:"number",default:0,min:0.01,max:2});
+ResolutionTools.attributes.add("ratio",{title:"Pixel Ratio",type:"number",default:1,min:0.01,max:2});
 ResolutionTools.attributes.add("smoothness",{title:"Smoothness",type:"number",default:0,enum:[
     {"NONE":-1},{"AUTO":0},{"PIXELATED":1},{"CRISP-EDGES":2}
 ]});
 ResolutionTools.attributes.add("fps",{title:"FPS",type:"number",default:60,min:0.5,max:60,step:1,precision:1});
-/*
-ResolutionTools.attributes.add("orientation",{title:"Set Orientation",type:"number",default:-1,enum:[
+
+ResolutionTools.attributes.add("orientation",{title:"Default setOrientation",type:"number",default:-1, description:
+    "The resolution to use when setOrientation function used without attributes.",
+                                              
+    enum:[
     {"none":-1},{"any":0}, {"natural":1}, {"landscape":2}, {"portrait":3} , {"portrait-primary":4} ,
     {"portrait-secondary":5} ,{"landscape-primary":6} ,{"landscape-secondary":7}
-]});*/
+]});
 
 //ResolutionTools.prototype.defaultFPS=window.requestAnimationFrame;
 // initialize code called once per entity
@@ -32,6 +35,9 @@ ResolutionTools.prototype.setResolutionRatio=function (ratio_=1){
     
 };
 
+/*
+ * Helps achieve pixelated effect. Disable Anti-Alias in Rendering settings for better pixelation effect
+ */ 
 ResolutionTools.prototype.setSmoothness=function(rendering_=-1){
     if (rendering_>-1)
         this.app.renderer.device.canvas.style.imageRendering=
@@ -42,17 +48,21 @@ ResolutionTools.prototype.toggleFullScreen=function(){
     this.app.graphicsDevice.fullscreen=!this.app.graphicsDevice.fullscreen;
 };
 
+
 ResolutionTools.prototype.setFullScreen=function(fullScreen_=true){
     this.app.graphicsDevice.fullscreen=fullScreen_;
 };
 /*
  * Change orientation to one of the following:  "any" "natural" "landscape" "portrait" 
  * "portrait-primary" "portrait-secondary" "landscape-primary" "landscape-secondary"
- * @param {string} type_ orientation type
+ * @param {string} [type_] orientation type
  
 
  */ 
-ResolutionTools.prototype.setOrientation=function(type_="any"){
+ResolutionTools.prototype.setOrientation=function(type_){
+    if(type_===undefined)
+        type_=this.orientation;
+    
     if (!isNaN(type_))
         if (type_==-1)return; else 
             type_=["any" ,"natural" ,"landscape" ,"portrait","portrait-primary", "portrait-secondary",
@@ -73,9 +83,3 @@ ResolutionTools.prototype.setFPS=function(fps){
         };  
     
 };
-// swap method called for script hot-reloading
-// inherit your script state here
-// ResolutionTools.prototype.swap = function(old) { };
-
-// to learn more about script anatomy, please read:
-// http://developer.playcanvas.com/en/user-manual/scripting/
